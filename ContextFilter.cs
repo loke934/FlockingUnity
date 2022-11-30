0ad659ca9f33f967134b60459db18d2d1d02fa85
Emilia Lokrantz <76095991+loke934@users.noreply.github.com>
2022-11-30 09:35:34 +0100

    Delete ContextFilter.cs

--- ContextFilter.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class ContextFilter : ScriptableObject
-{
-    public abstract List<Transform> Filter(Boid boid, List<Transform> original);
-}
